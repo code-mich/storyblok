bec4246ae7de3fa2ce68523d2a7dbb4f7ce31117
michelle <michellewong@rumie.org>
2022-11-01 10:44:43 -0400

    setup storyblok and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    modules: [
+        '@nuxtjs/tailwindcss',
+      [
+        '@storyblok/nuxt',
+        {
+                  accessToken: 'X2nlnfYMkVnwBbDGtyHP6Qtt',
+                  // If Server Location === US then:
+                  apiOptions: { region: 'us' }
+              }
+      ]
+    ],
+  })
